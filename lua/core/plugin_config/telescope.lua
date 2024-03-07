ba5d470bcde78a7eb592214e0628540ea8b8fbf8
Adrian Istrati <adrian.istrati-ext@aviv-group.com>
2024-03-07 16:38:35 +0200

    new telescope keymaps

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -13,9 +13,14 @@ function call(callback, settings)
 	end
 end
 
-vim.keymap.set("n", "<leader><space>", call(builtin.find_files, filesViewSettings), {})
+vim.keymap.set("n", "<leader><space>", call(builtin.buffers, filesViewSettings), {})
 vim.keymap.set("n", "<leader>fr", call(builtin.oldfiles, filesViewSettings), {})
 vim.keymap.set("n", "<leader>ff", call(builtin.find_files, filesViewSettings), {})
 vim.keymap.set("n", "<leader>sg", call(builtin.live_grep, viewWithContentSettings), {})
 vim.keymap.set("n", "<leader>sh", call(builtin.help_tags, viewWithContentSettings), {})
-vim.keymap.set("n", "<leader>fb", call(builtin.buffers, filesViewSettings), {})
+vim.keymap.set("n", "<leader>s/", function()
+	builtin.live_grep({
+		grep_open_files = true,
+		prompt_title = "Live Grep in Open Files",
+	})
+end, { desc = "[S]earch [/] in Open Files" })
