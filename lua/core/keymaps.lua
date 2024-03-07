4fb3bb7da251a2647fd1ee74f8aea6d125b30f06
Adrian Istrati <adrian.istrati-ext@aviv-group.com>
2024-03-07 16:37:55 +0200

    move basic vim configuration

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,14 +1,1 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.cursorline = true
-vim.opt.autoread = true
-vim.opt.shiftround = true
-
-vim.wo.relativenumber = true
-vim.wo.number = true
-vim.opt.clipboard = "unnamedplus"
-
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
