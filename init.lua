10f76b0ff1f7f2dd12d1f11187216287e1a6f7dd
Adrian Istrati <adrian.istrati-ext@aviv-group.com>
2024-04-26 16:39:45 +0300

    set indentation size to 2

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = " "
 
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.shiftround = true
